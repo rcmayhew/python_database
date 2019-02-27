7db7e64ba2af4267e77c6829e41fa3dfd8d927a8
rcmayhew <rcmayhewii@gmail.com>
2019-02-26 22:11:17 -0800

    added in loading the database

--- builder.py
+++ builder.py
@@ -1,5 +1,5 @@
 import os
-
+#need to add database meta data to file
 
 class Builder:
     def __init__(self, master_dir=os.getcwd(),
@@ -7,21 +7,38 @@ class Builder:
         self.master_folder = master_dir
         self.loaded_databases = prior_database_file
         self.databases = {}
+        self.load_databases()
 
+    #finds the list of databases in list_of_db.py
+    #it should be in the main folder of the database
     def load_databases(self):
         if self.loaded_databases is None:
             self.databases = {}
         else:
-
-
-
+            try:
+                import list_of_db as valid_dbs
+                self.databases = valid_dbs.db
+            except ImportError:
+                print("No valid databases at %s" %
+                      self.loaded_databases)
+                self.databases = {}
 
     #def check database exist
     def database_exist(self, database_name):
-        return database_name in database_names
+        return database_name in self.databases
 
     #edit __init.py__ list
-    def edit_all(self, database_name):
+    def edit_database_list(self, database_name):
         if self.database_exist(database_name):
             pass
-    #make the weapon database
+        else:
+            #will remove the closing bracket and then add the
+            #needed info
+            pass
+
+    #add data to the list
+    #can pull data, amend, then reprint
+    #would rather amend the print, but error checking will be hard
+    def amend_database(self):
+        #need to pull the prior database or atleast pull the shape
+        pass
